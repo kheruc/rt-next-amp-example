c4acbe27f6c4597b2cf4a8517bce43c037570e3c
Kristian Heruc <kristian@heruc.com>
2019-04-27 12:51:00 +0200

    Add posts to the homepage

---
+++ components/post.js
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import authors from '../data/authors';
+
+const Post = ({ post }) => {
+  let author = authors.find(author => author.id == post.id);
+  return (
+    <div className="post">
+      <h2>{post.title}</h2>
+      <div className="author">
+        <amp-img width={35} height={35} className="avatar" src={`/static/avatars/${author.gender}/${author.photo}`} />{' '}
+        <strong>{author.name}</strong>
+      </div>
+      <div className="category">
+        <Link href={`/category?id=${post.category.id}`} as={`/category/${post.category.id}`}>
+          <a>{post.category.title}</a>
+        </Link>
+      </div>
+      <div className="text">
+        {post.body}
+        {'... '}
+        <Link href={`/post?id=${post.id}`} as={`/post/${post.id}`}>
+          <a className="readmore">read more</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        .post {
+          margin-bottom: 60px;
+        }
+        .post h2 {
+          text-transform: capitalize;
+        }
+        .avatar {
+          float: left;
+          width: 35px;
+          height: 35px;
+          margin: 0 10px 10px 0;
+          border-radius: 50%;
+        }
+        .author {
+          color: #333;
+        }
+        .category {
+          clear: left;
+        }
+        .category a {
+          font-size: 1.3rem;
+          color: #555;
+          background: #eee;
+          padding: 2px 6px;
+          border-radius: 3px;
+        }
+        .category a:hover {
+          background: #ddd;
+        }
+        .readmore {
+          font-size: 1.6rem;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Post;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import { withAmp } from 'next/amp';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/layout';
+import Post from '../components/post';
+import categories from '../data/categories.json';
 
-const Index = () => (
-  <Layout>
-    <h1>Next.js AMP Example</h1>
-  </Layout>
-);
+const Index = ({ posts }) => {
+  return (
+    <Layout>
+      <div className="post-list">
+        {posts.map(post => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+Index.getInitialProps = async () => {
+  // fetch list of posts
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=1`);
+  const posts = await res.json();
+  // assign a random category
+  posts.map(post => {
+    return (post.category = categories[Math.floor(Math.random() * categories.length)]);
+  });
+  return { posts };
+};
 
 export default withAmp(Index);
